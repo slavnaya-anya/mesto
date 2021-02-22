253dcae3cfc937e0e63794e5d4f092b80dd44efa
Anna Orlova <slavnaya_anyaa@yandex.ru>
2021-02-22 13:49:22 +0300

    Проектная работа 4

---
+++ script/index.js
@@ -0,0 +1,51 @@
+let openButton = document.querySelector('.popup_open');
+let body = document.querySelector('.body');
+let overlay = document.querySelector('.overlay');
+let userName = document.querySelector('.profile__name');
+let userAbout = document.querySelector('.profile__inform');
+let closeButton = overlay.querySelector('.popup__close-button');
+
+
+
+let form = document.forms.edituser;
+let inputName = form.elements.name;
+let inputAbout = form.elements.about;
+
+function closePopup() {
+  overlay.classList.remove('overlay_active');
+}
+
+function editUserInfo() {
+  userName.textContent = inputName.value;
+  userAbout.textContent = inputAbout.value;
+}
+
+openButton.addEventListener('click', () => {
+  overlay.classList.add('overlay_active');
+});
+
+closeButton.addEventListener('click', closePopup) ;
+
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  editUserInfo();
+  closePopup();
+});
+
+document.addEventListener('keypress', function(event){
+  if (event.keyCode === 32) {
+    const popupActive = document.querySelector('.popup_open');
+    closePopup(popupActive);
+    event.preventDefault();
+  }
+})
+
+
+
+overlay.addEventListener('click', (event) => {
+  if (event.target === event.currentTarget) {
+    closePopup();
+  }
+});
